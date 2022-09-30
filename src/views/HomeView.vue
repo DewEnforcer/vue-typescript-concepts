6dab1ebbe4de97c2ad2043e52a804b31f0e43d45
DewEnforcer <piranha863@gmail.com>
2022-09-30 13:44:12 +0200

    Another composition api example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,34 @@
 <template>
-  <h1>Hi</h1>
-  <span>{{name}} - {{age}}</span>
-  <button type="button" @click="changeName('MyName')">Change name</button>
-  <button type="button" @click="changeAge('25')">Change age</button>
+  <div>
+    
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-
+import Job from "../types/Job";
 export default defineComponent({
   name: 'HomeView',
   components: {
-    
+      
   },
   setup() {
-      const name = ref("Link");
-      const age = ref<number | string | null>(30);
+    const jobs = ref<Job[]>([
+      {
+        title: "webdev",
+        location: "Brno",
+        salary: "Many moneys",
+        id: "1234-ABCD"
+      },
+      {
+        title: "backend dev",
+        location: "Brno",
+        salary: "Many moneys++",
+        id: "1234-EFGH"
+      },
+    ]);
 
-      const changeName = (nameArg: string) => {
-        name.value = nameArg;
-      }
-      const changeAge = (ageArg: string | number) => {
-        age.value = ageArg;
-      }
-
-      return {name, age, changeName, changeAge}
+    return {jobs}
   },
 });
 </script>
