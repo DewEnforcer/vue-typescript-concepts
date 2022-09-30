e400395ce1bf43dab5f7f4ca6d1b6493fdc76247
DewEnforcer <piranha863@gmail.com>
2022-09-30 13:54:02 +0200

    Props and typescript example

---
+++ src/components/JobList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="job in jobs" :key="job.id" class="job-item">
+            <span>{{job.title}}</span>
+            <span>{{job.location}}</span>
+            <span>{{job.salary}}</span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts"> //tsvue
+import Job from '@/types/Job'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        jobs: {
+            required: true,
+            type: Array as PropType<Job[]>
+        }
+    },
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Job from "../types/Job";
+import JobList from "../components/JobList.vue";
+
 export default defineComponent({
   name: 'HomeView',
   components: {
-      
+      JobList
   },
   setup() {
     const jobs = ref<Job[]>([
